9a5e81221ff0e64553031358f1323aaeefe11410
vitor <vitor.grosskopf@gmail.com>
2024-08-05 23:22:20 -0300

    First commit - Add project folder - Add main file

---
+++ seconds_transform/lib/seconds_app.rb
@@ -0,0 +1,12 @@
+def seconds_transform(s)
+
+end
+
+# 365 days == 31_536_000 s
+# 1 min == 60 s
+# 60 min == 3600 s
+#   1 hour == 60 min
+# 1440 min == 86400 s
+#   1 day == 24 hour
+#   24 hour == 1440 min
+# 1 year == 365 days
